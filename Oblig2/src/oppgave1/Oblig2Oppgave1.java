c2145a0510f2394d281a0831d6439f04ca97f23b
Sebbern <Sebastianeckbo@gmail.com>
2022-10-15 18:01:38 +0200

    add while-loop

--- Oblig2/src/oppgave1/Oblig2Oppgave1.java
+++ Oblig2/src/oppgave1/Oblig2Oppgave1.java
@@ -4,13 +4,26 @@ import javax.swing.*;
 
 public class Oblig2Oppgave1 {
     public static void main(String[] args) {
-        int nedreGrense = Integer.parseInt(JOptionPane.showInputDialog("Skriv inn den nedre grensen."));
-        int ovreGrense = Integer.parseInt(JOptionPane.showInputDialog("Skriv inn den øvre grensen."));
+        int nedreGrense;
+        int ovreGrense;
         int sum = 0;
+        int count = 0;
+
+        //Hvis den øvre verdien er lik eller lavere enn den nedre verdien vil while-løkken og if-setningen tvinge brukeren til å lese inn en ny verdi.
+        while (true) {
+            nedreGrense = Integer.parseInt(JOptionPane.showInputDialog("Skriv inn den nedre grensen."));
+            ovreGrense = Integer.parseInt(JOptionPane.showInputDialog("Skriv inn den øvre grensen."));
+            if (nedreGrense >= ovreGrense) {
+                JOptionPane.showMessageDialog(null, "Den øvre grensen er lik, eller er mindre enn den nedre grensen. Gjenta innlesning.");
+            }
+            else{
+                break;
+            }
+        }
 
         for (int i = nedreGrense; i <= ovreGrense; i++){
             //Lager ny line break etter hvert 10 tall og printer ut det første tallet i en rekke
-            if ((i%10) == 1){
+            if ((count%10) == 0){
                 System.out.print("\n"+i+" + ");
             }
             //Sjekker om det siste tallet er på vei til å bli skrevet ut, printer deretter det siste tallet og ett "=" tegn
@@ -21,6 +34,7 @@ public class Oblig2Oppgave1 {
             else {
                 System.out.print(i+" + ");
             }
+            count += 1;
             sum += i;
         }
         System.out.print(sum);
